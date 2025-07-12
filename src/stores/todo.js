07afaca7eda06feb5f40344e278b8120514e862e
phuriphat-w <phuriphat.wong@gmail.com>
2025-07-13 01:54:27 +0700

    Create store using Pinia

--- src/stores/todo.js
+++ src/stores/todo.js
@@ -1,12 +1,58 @@
-import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+import axios from 'axios'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
-  }
+const BASE_URL = 'https://687276eb76a5723aacd4d0ce.mockapi.io'
 
-  return { count, doubleCount, increment }
+export const useTodoStore = defineStore('todo', {
+  state: () => ({
+    list: [],
+    selectedTodo: {},
+    status: ['Pending', 'Doing', 'Done'],
+  }),
+  actions: {
+    async loadTodos() {
+      try {
+        const response = await axios.get(`${BASE_URL}/todos`)
+        this.list = response.data
+      } catch (error) {
+        console.log('Error', error)
+      }
+    },
+    async loadTodo(id) {
+      try {
+        const response = await axios.get(`${BASE_URL}/todos/${id}`)
+        this.selectedTodo = response.data
+      } catch (error) {
+        console.log('Error', error)
+      }
+    },
+    async addTodo(todoText) {
+      const bodyData = {
+        name: todoText,
+        status: 'Pending',
+      }
+      try {
+        const response = await axios.post(`${BASE_URL}/todos/`, bodyData)
+        console.log('Added ', response.data)
+      } catch (error) {
+        console.log('Error', error)
+      }
+    },
+    async editTodo(todoData, id) {
+      try {
+        const response = await axios.put(`${BASE_URL}/todos/${id}`, todoData)
+        console.log('Edit completed', response.data)
+      } catch (error) {
+        console.log('Error', error)
+      }
+    },
+    async removeTodo(id) {
+      try {
+        const response = await axios.delete(`${BASE_URL}/todos/${id}`)
+        console.log('Removed', response.data)
+      } catch (error) {
+        console.log('Error', error)
+      }
+    },
+  },
 })
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,1 +1,12 @@
+<script setup>
+import { useTodoStore } from '../stores/todo'
+import { onMounted } from 'vue'
+
+const todoStore = useTodoStore()
+
+onMounted(async () => {
+  await todoStore.loadTodos()
+  console.log(todoStore.list)
+})
+</script>
 <template></template>
